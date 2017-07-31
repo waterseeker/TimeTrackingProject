17cc80ed30ff56ad9d91da7c043d4084f6e3d4af
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-31 15:49:28 -0500

    initial commit

---
+++ ProjectTimeTracking/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTimeTracking.Models
+{
+    public enum EmploymentType
+    {
+        FullTime,
+        PartTime,
+        Contract,
+        Intern
+    }
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string EmployeeFirstMidName { get; set; }
+
+        public string EmployeeLastName { get; set; }
+
+        public EmploymentType KindOfEmployment{ get; set; }
+
+        public  string  EmployeeUserName { get; set; }
+
+        public string EmployeePassword { get; set; }
+       
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
