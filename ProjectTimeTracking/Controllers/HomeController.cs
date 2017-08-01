20fbaeba32c7d34781ebd83a2474a0474f637056
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-01 11:12:49 -0500

    added logic to show num employees in each type of employment on the about page.

--- ProjectTimeTracking/Controllers/HomeController.cs
+++ ProjectTimeTracking/Controllers/HomeController.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectTimeTracking.Data;
+using ProjectTimeTracking.Models.ProjectViewModels;
 
 namespace ProjectTimeTracking.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ProjectTimeTrackingContext _context;
+
+        public HomeController(ProjectTimeTrackingContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EmploymentTypeGroup> data =
+                from employee in _context.Employees
+                group employee by employee.KindOfEmployment into employmentGroup
+                select new EmploymentTypeGroup()
+                {
+                    KindOfEmployment = employmentGroup.Key,
+                    EmployeeCount = employmentGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
---
+++ ProjectTimeTracking/Models/ProjectViewModels/EmploymentTypeGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTimeTracking.Models.ProjectViewModels
+{
+    public class EmploymentTypeGroup
+    {
+
+        [EnumDataType(typeof(EmploymentType))]
+        public EmploymentType KindOfEmployment { get; set; }
+
+        public int EmployeeCount { get; set; }
+    }
+}
