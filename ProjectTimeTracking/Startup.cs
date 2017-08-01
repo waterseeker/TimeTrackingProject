52aa1c718cfce6f80e2fcde824f19f539c58160d
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-01 07:18:29 -0500

    added ConnectionStrings to appsettings.json

--- ProjectTimeTracking/Startup.cs
+++ ProjectTimeTracking/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ProjectTimeTracking.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjectTimeTracking
 {
@@ -28,6 +30,9 @@ namespace ProjectTimeTracking
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+            services.AddDbContext<ProjectTimeTrackingContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
             services.AddMvc();
         }
 
