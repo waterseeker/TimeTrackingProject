1b1de78d525ddff7412b7e6616557e62d1043dcf
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-01 09:53:23 -0500

    Got the drop down list to populate from the enum but setting typeof ont he enum in the model and using the asp-items helper for the prop

--- ProjectTimeTracking/Models/Employee.cs
+++ ProjectTimeTracking/Models/Employee.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
+
 
 namespace ProjectTimeTracking.Models
 {
-    public enum EmploymentType
-    {
-        FullTime,
-        PartTime,
-        Contract,
-        Intern
-    }
+
+
     public class Employee
     {
         public int ID { get; set; }
@@ -19,7 +22,7 @@ namespace ProjectTimeTracking.Models
         public string EmployeeFirstName { get; set; }
 
         public string EmployeeLastName { get; set; }
-
+        [EnumDataType(typeof(EmploymentType))]
         public EmploymentType KindOfEmployment{ get; set; }
 
         public  string  EmployeeUserName { get; set; }
@@ -27,5 +30,17 @@ namespace ProjectTimeTracking.Models
         public string EmployeePassword { get; set; }
        
         public ICollection<TimeEntry> TimeEntries { get; set; }
+    }
+
+    public enum EmploymentType
+    {
+        [Display(Name = "FullTime")]
+        FullTime,
+        [Description("Part Time")]
+        PartTime,
+        [Description("Contract")]
+        Contract,
+        [Description("Intern")]
+        Intern
     }
 }
