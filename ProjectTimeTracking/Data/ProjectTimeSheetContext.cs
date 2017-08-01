02dd458a38072308df1a5f724b34b9ab4234735b
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-01 07:07:15 -0500

    added Data folder and ProjectTimeSheetContext.cs file in that.

---
+++ ProjectTimeTracking/Data/ProjectTimeSheetContext.cs
@@ -0,0 +1,16 @@
+﻿using ProjectTimeTracking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectTimeTracking.Data
+{
+    public class ProjectTimeTrackingContext : DbContext
+    {
+        public ProjectTimeTrackingContext(DbContextOptions<ProjectTimeTrackingContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
